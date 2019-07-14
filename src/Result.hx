d0c9ddb038c528428906d122b129962252d76a7b
Dmitry Hryppa <dmitry.hryppa@gmail.com>
2019-07-14 03:47:31 +0300

    improve Result

--- src/Result.hx
+++ src/Result.hx
@@ -1,11 +1,29 @@
 package;
 
+import haxe.PosInfos;
+import better.Log;
+
 /**
 	Result is a wrapper type which can be `Success` and contain optional data in it
 	or `Failure` and contain failure message.
 	It is similar to `haxe.ds.Optional`.
-	*/
+ */
+@:using(Result.ResultExt)
 enum Result<T> {
 	Success(?data:T);
 	Failure(errorMessage:String);
+}
+
+class ResultExt {
+	public static inline function log<T>(resp:Result<T>, ?pos:PosInfos):Result<T> {
+		switch (resp) {
+			case Failure(msg):
+				Log.error(msg, pos);
+			case Success(data):
+				if (data != null) {
+					Log.success(data);
+				}
+		};
+		return resp;
+	}
 }
