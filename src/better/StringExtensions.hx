0c9bdb4ddcba740ca12df954429146b95653f99c
Dmitry Hryppa <dmitry.hryppa@gmail.com>
2022-03-28 00:41:31 +0300

    Add upperFirst for string

--- src/better/StringExtensions.hx
+++ src/better/StringExtensions.hx
@@ -2,6 +2,8 @@ package better;
 
 import haxe.Json;
 
+using StringTools;
+
 @:publicFields
 final class StringExtensions{
 	static inline function isEmpty(value:String):Bool {
@@ -14,5 +16,22 @@ final class StringExtensions{
 
 	static inline function parseJson(value:String):Any {
 		return Json.parse(value);
+	}
+
+	/**
+	 * Converts the first character of string to upper case.
+	 * @param str The string to convert.
+	 * @return String
+	 */
+	 static inline function upperFirst(str:String):String {
+		if (str.length == 0 || str.isSpace(0)) {
+			return str;
+		}
+
+		if (str.length == 1) {
+			return str.toUpperCase();
+		}
+
+		return str.charAt(0).toUpperCase() + str.substr(1);
 	}
 }
--- tests/tests/StringTests.hx
+++ tests/tests/StringTests.hx
@@ -2,23 +2,29 @@ package tests;
 
 import utest.Assert;
 import utest.Test;
+
 using better.StringExtensions;
 
 final class StringTests extends Test {
-	public function test_IsEmpty() {
+	function test_IsEmpty():Void {
 		final str1 = "";
 		final str2 = "test";
 		Assert.isTrue(str1.isEmpty() && !str2.isEmpty());
 	}
 
-	public function test_IsNotEmpty() {
+	function test_IsNotEmpty():Void {
 		final str1 = "";
 		final str2 = "test";
 		Assert.isTrue(!str1.isNotEmpty() && str2.isNotEmpty());
 	}
 
-	public function test_parseJson() {
+	function test_parseJson():Void {
 		final obj:{hello:String} = '{"hello":"World"}'.parseJson();
 		Assert.equals(obj.hello, "World");
+	}
+
+	function test_upperFirst():Void {
+		final str = 'hello';
+		Assert.equals('Hello', str.upperFirst());
 	}
 }
